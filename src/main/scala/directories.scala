a48a9d70980c1059d0d958f5ff31cc8b7ab10661
Jonathan Julian Huerta y Munive <jonjulian23@gmail.com>
2024-08-28 17:27:04 +0200

    More robust functions for data extraction.

---
+++ src/main/scala/directories.scala
@@ -0,0 +1,10 @@
+package isabelle_rl
+
+object Directories {
+  private val run_main = ""
+  val isabelle_repo = ""
+  val isabelle_app = ""
+  val isabelle_afp = ""
+  val test_dir = ""
+  val isabelle_rl = ""
+}
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,10 +3,11 @@ package isabelle_rl
 import java.nio.file.{Path, Paths}
 
 import de.unruh.isabelle.control.{Isabelle, OperationCollection}
-import de.unruh.isabelle.mlvalue.MLValue.{compileFunction, compileFunction0}
+import de.unruh.isabelle.mlvalue.MLValue.{compileValue, compileFunction, compileFunction0}
 import de.unruh.isabelle.mlvalue.{MLValue, MLFunction, MLFunction0, MLFunction2, MLFunction3}
 import de.unruh.isabelle.pure.{Abs, App, Const, Term, Transition, Context, Theory}
 import isabelle_rl.TheoryManager._
+import isabelle_rl.Directories
 
 // Implicits
 import de.unruh.isabelle.mlvalue.Implicits._
@@ -16,13 +17,19 @@ import de.unruh.isabelle.pure.Implicits._
 object Main {
   def main (args: Array[String]): Unit = {
     val setup = Isabelle.Setup(
-      isabelleHome = Path.of("..."), 
+      isabelleHome = Path.of(Directories.isabelle_repo), 
       logic = "HOL",
-      workingDirectory = Paths.get("...")
+      workingDirectory = Paths.get(Directories.test_dir)
       )
     implicit val isabelle: Isabelle = new Isabelle(setup)
 
-    val theory_path = "Test_Thy3"
+    val isabelle_rl_thy : Theory = Theory(Path.of(Directories.isabelle_rl + "Isabelle_RL.thy"))
+    val rl_ops : String = isabelle_rl_thy.importMLStructureNow("Ops")
+    // val Actions : String = isabelle_rl_thy.importMLStructureNow("Actions")
+    val read_file : MLFunction[String, String] = compileFunction(s"${rl_ops}.read_file")
+    
+    println(read_file(Directories.test_dir + "Test_Thy3.thy").retrieveNow)
+    /*
     val some_manager = new TheoryManager()
     println("we have successfully loaded a theory manager")
 
@@ -40,6 +47,7 @@ object Main {
     println("we have defined the theory name")
 
     println(get_theory_name(thy).retrieveNow)
+    */
   }
 }
 
