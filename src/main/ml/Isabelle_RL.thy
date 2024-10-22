83782261a9cd53c7c56d292bb9323140266061cb
Jonathan Julian Huerta y Munive <jonjulian23@gmail.com>
2024-10-22 12:23:18 +0200

    Reorganised files.

---
+++ src/main/ml/Isabelle_RL.thy
@@ -0,0 +1,17 @@
+theory Isabelle_RL
+  imports Pure
+begin
+
+ML_file "pred.ML"
+ML_file "ops.ML"
+ML_file "get.ML"
+ML_file "print.ML"
+ML_file "sections.ML"
+ML_file "seps.ML"
+ML_file "actions.ML"
+ML_file "data.ML"
+ML_file "json_maker.ML"
+ML_file "g2tac_formatter.ML"
+ML_file "writer.ML"
+
+end
