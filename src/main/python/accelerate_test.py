b03b493453faed089b216858a5dc710934ed382f
Jonathan Julian Huerta y Munive <jonjulian23@gmail.com>
2025-01-27 23:47:47 +0100

    More accelerate tests

--- src/main/python/accelerate_test.py
+++ src/main/python/accelerate_test.py
@@ -3,7 +3,10 @@ from accelerate import Accelerator
 
 def main():
     accelerator = Accelerator()
-    print(torch.cuda.is_available())
+    device = accelerator.device
+    print(f"Process {accelerator.process_index}: Using device {device}")
+    print(f"torch.cuda.is_available(): {torch.cuda.is_available()}")
+    print(f"torch.cuda.current_device(): {torch.cuda.current_device() if torch.cuda.is_available() else 'No CUDA available'}")
 
 if __name__ == "__main__":
     main()
