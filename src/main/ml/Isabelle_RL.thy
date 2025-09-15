ddfe11c580c22e94cffe9de210f87cbb8ef02abe
Jonathan Julian Huerta y Munive <jonjulian23@gmail.com>
2025-09-16 00:07:41 +0200

    Simple command calling LLM from Isabelle

--- src/main/ml/Isabelle_RL.thy
+++ src/main/ml/Isabelle_RL.thy
@@ -1,5 +1,6 @@
 theory Isabelle_RL
   imports Pure
+  keywords "llm_recommend" :: diag
 begin
 
 ML_file "pred.ML"
@@ -19,5 +20,28 @@ ML_file "data.ML"
 ML_file "json_maker.ML"
 ML_file "g2tac_formatter.ML"
 ML_file "writer.ML"
+
+ML \<open>
+val _ =
+  Outer_Syntax.command \<^command_keyword>\<open>llm_recommend\<close>
+    "quries a recommendation from a running llm"
+    ((Resources.parse_file -- Parse.int) 
+      >> (fn (get_file, num) => 
+        Toplevel.keep_proof (
+          fn state => 
+          let
+            val _ = Client.connect_to_server 5006;
+            val thy = Toplevel.theory_of state;
+            val file_path = Path.implode (#src_path (get_file thy));
+            val prf_data = Json_Maker.llm_proof_data file_path state num;
+            val _ = Client.communicate prf_data;
+            val response = Client.get_last_response ();
+            val _ = Client.disconnect ();
+            val _ = (case response of
+              SOME sugg => Output.information (Active.sendback_markup_properties [] sugg)
+              | NONE => Output.writeln "NONE suggestion from LLM")
+          in () end))
+    );
+\<close>
 
 end
