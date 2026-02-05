8fc2091a649115cbca396d3ce0a45a0b8b74af79
Jonathan Julian Huerta y Munive <jonjulian23@gmail.com>
2026-02-05 15:02:52 +0100

    Returning directories.scala to generic instructions

--- src/main/scala/directories.scala
+++ src/main/scala/directories.scala
@@ -8,7 +8,7 @@ Isabelle/RL directories: Adjust for your specific setup
 package isabelle_rl
 
 object Directories {
-  val isabelle_app = "/Applications/Isabelle2025-2.app/"      // a bin directory should be there
-  val isabelle_afp = "/Users/jonathan/Programs/isabelle/afp/thys/" // a ROOTS file should be there
-  val isabelle_rl = "/Users/jonathan/Programs/deepIsaHOL/"
+  val isabelle_app = "/path/to/your/isabelle/app/"      // a bin directory should be there
+  val isabelle_afp = "/path/to/your/isabelle/afp/thys/" // a ROOTS file should be there
+  val isabelle_rl = "/path/to/this/project/"            // a src directory should be there
 }
